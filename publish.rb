afa5b07e7c5ad5a6c1521d9c07794765e55c59ba
Weltraumschaf <ich@weltraumschaf.de>
2012-03-17 17:05:53 +0100

    Move stuff in own module and add css.

--- publish.rb
+++ publish.rb
@@ -1,61 +1,24 @@
+require 'uberblog'
 require 'erb'
-require 'kramdown'
-require 'uri'
-require 'pathname'
 
-class BlogPost
-    def initialize(title, content, date)
-        @title = title
-        @content = content
-        @date = date
-    end
-
-    def getBinding
-        binding
-    end
-
-    def generateFilename
-        URI.escape "#{@title.gsub(' ', '-').gsub('/', '%2F')}.html"
-    end
-
-    def to_s
-        "<BlogPost: #{@title}>"
-    end
-end
-
-class BlogData
-    def initialize(filename)
-        @filename = filename
-        content = File.open(filename, "rb") { |file| file.read }
-        @doc = Kramdown::Document.new(content)
-    end
-
-    def title
-        @doc.root.children[0].children[0].value
-    end
-
-    def html
-        @doc.to_html
-    end
-
-    def date
-        basename = Pathname.new(@filename).basename.to_s
-        datePart = basename[0, basename.index('_')]
-    end
-
-    def to_s
-        "<BlogData: #{title}>"
-    end
-end
-
-template = File.open("./template.rhtml", "rb") { |file| file.read }
+# crate the blog posts
+template = File.open("./templates/post.rhtml", "rb") { |file| file.read }
 rhtml = ERB.new(template)
+posts = Uberblog::BlogPostList.new
 
 Dir.foreach("./data") do |file|
     next if file == '.' or file == '..'
-    data = BlogData.new("./data/#{file}")
-    post = BlogPost.new(data.title, data.html, data.date)
-    generated = File.new("./htdocs/#{post.generateFilename}", 'w')
+    data = Uberblog::BlogData.new("./data/#{file}")
+    post = Uberblog::BlogPost.new(data.title, data.html, data.date)
+    generated = File.new("./htdocs/#{post.filename}", 'w')
     generated.write rhtml.result(post.getBinding)
     generated.close
+    posts.append(post)
 end
+
+#create the index
+template = File.open("./templates/index.rhtml", "rb") { |file| file.read }
+rhtml = ERB.new(template)
+#create the feeds
+
+#create the google site map
---
+++ uberblog.rb
@@ -0,0 +1,70 @@
+require 'kramdown'
+require 'uri'
+require 'pathname'
+
+module Uberblog
+    class BlogPost
+        attr_reader :title, :date
+
+        def initialize(title, content, date)
+            @title = title
+            @content = content
+            @date = date
+        end
+
+        def getBinding
+            binding
+        end
+
+        def filename
+            URI.escape "#{@title.gsub(' ', '-').gsub('/', '%2F')}.html"
+        end
+
+        def to_s
+            "<BlogPost: #{@title}>"
+        end
+    end
+
+    class BlogPostList
+        def initialize
+            @posts = Array.new
+        end
+
+        def append(aBlogPost)
+            @posts.push(aBlogPost)
+            self
+        end
+
+        def [](key)
+            return @posts[key] if key.kind_of?(Integer)
+            return nil
+        end
+    end
+
+    class BlogData
+        def initialize(filename)
+            @filename = filename
+            content = File.open(filename, "rb") { |file| file.read }
+            @doc = Kramdown::Document.new(content)
+        end
+
+        def title
+            @doc.root.children[0].children[0].value
+        end
+
+        def html
+            @doc.to_html
+        end
+
+        def date
+            basename = Pathname.new(@filename).basename.to_s
+            dateParts = basename[0, basename.index('_')].split(/-/)
+            date = Time.utc(dateParts[0], dateParts[1], dateParts[2])
+            date.strftime('%d.%m.%Y')
+        end
+
+        def to_s
+            "<BlogData: #{title}>"
+        end
+    end
+end
