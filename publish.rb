c12b3d1a81a36e5d5b3fa279c0ff1317b1f0eb50
Weltraumschaf <ich@weltraumschaf.de>
2012-03-18 00:26:19 +0100

    Add style stuff from my old wordpress.

--- publish.rb
+++ publish.rb
@@ -2,28 +2,34 @@ require 'uberblog'
 require 'erb'
 require 'rss/maker'
 require 'find'
+require 'pathname'
 
+# config stuff
 $siteUrl = 'http://localhost/~sxs/uberblog/'
+$baseDir = Pathname.pwd.to_s
+$dataDir = "#{$baseDir}/data"
+$tplDir  = "#{$baseDir}/templates"
+$htdocs  = "#{$baseDir}/htdocs"
 
 # crate the blog posts
-template = File.open("./templates/post.rhtml", "rb") { |file| file.read }
+template = File.open("#{$tplDir}/post.erb", "rb") { |file| file.read }
 rhtml    = ERB.new(template)
 list     = Uberblog::BlogPostList.new($siteUrl)
 
-Dir.foreach("./data") do |file|
+Dir.foreach($dataDir) do |file|
     next if file == '.' or file == '..'
-    data = Uberblog::BlogData.new("./data/#{file}")
+    data = Uberblog::BlogData.new("#{$dataDir}/#{file}")
     post = Uberblog::BlogPost.new(data.title, data.html, data.date, $siteUrl)
     html = rhtml.result(post.getBinding)
-    generated = File.open("./htdocs/#{post.filename}", 'w') { |file| file.write(html) }
+    File.open("#{$htdocs}/#{post.filename}", 'w') { |file| file.write(html) }
     list.append(post)
 end
 
 #create the index
-template = File.open("./templates/index.rhtml", "rb") { |file| file.read }
+template = File.open("#{$tplDir}/index.erb", "rb") { |file| file.read }
 rhtml    = ERB.new(template)
 index    = rhtml.result(list.getBinding)
-File.open("./htdocs/index.html", 'w') { |file| file.write(index) }
+File.open("#{$htdocs}/index.html", 'w') { |file| file.write(index) }
 
 #create the feeds
 feed = RSS::Maker.make('2.0') do |maker|
@@ -42,16 +48,16 @@ feed = RSS::Maker.make('2.0') do |maker|
         item.date          = Time.parse(post.date)
     end
 end
-File.open("./htdocs/feed.xml","w") { |file| file.write(feed) }
+File.open("#{$htdocs}/feed.xml","w") { |file| file.write(feed) }
 
 #create the google site map
-template = File.open("./templates/sitemap.erb", "rb") { |file| file.read }
+template = File.open("#{$tplDir}/sitemap.erb", "rb") { |file| file.read }
 rhtml    = ERB.new(template)
 sitemap  = Uberblog::SiteMap.new($siteUrl)
-Find.find("./htdocs") do |file|
+Find.find($htdocs) do |file|
     if file =~ /.html$/
         sitemap.append(file)
     end
 end
 xml = rhtml.result(sitemap.getBinding)
-File.open("./htdocs/sitemap.xml","w") { |f| f.write(xml) }
+File.open("#{$htdocs}/sitemap.xml","w") { |f| f.write(xml) }
