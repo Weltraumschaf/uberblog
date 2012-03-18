bafaa9dd693152b49013d31fc0d5ec5f4f7baffa
Weltraumschaf <ich@weltraumschaf.de>
2012-03-18 03:14:38 +0100

    Fix the layout.

--- uberblog.rb
+++ uberblog.rb
@@ -3,6 +3,33 @@ require 'uri'
 require 'pathname'
 
 module Uberblog
+    class BlogData
+        def initialize(filename)
+            @filename = filename
+            content = File.open(filename, "rb") { |file| file.read }
+            @doc = Kramdown::Document.new(content)
+        end
+
+        def title
+            @doc.root.children[0].children[0].value
+        end
+
+        def html
+            @doc.to_html
+        end
+
+        def date
+            basename  = Pathname.new(@filename).basename.to_s
+            dateParts = basename[0, basename.index('_')].split(/-/)
+            date      = Time.utc(dateParts[0], dateParts[1], dateParts[2])
+            date.strftime('%d.%m.%Y')
+        end
+
+        def to_s
+            "<BlogData: #{title}>"
+        end
+    end
+
     class BlogPost
         attr_reader :title, :date, :content, :siteUrl
 
@@ -41,33 +68,6 @@ module Uberblog
 
         def getBinding
             binding
-        end
-    end
-
-    class BlogData
-        def initialize(filename)
-            @filename = filename
-            content = File.open(filename, "rb") { |file| file.read }
-            @doc = Kramdown::Document.new(content)
-        end
-
-        def title
-            @doc.root.children[0].children[0].value
-        end
-
-        def html
-            @doc.to_html
-        end
-
-        def date
-            basename  = Pathname.new(@filename).basename.to_s
-            dateParts = basename[0, basename.index('_')].split(/-/)
-            date      = Time.utc(dateParts[0], dateParts[1], dateParts[2])
-            date.strftime('%d.%m.%Y')
-        end
-
-        def to_s
-            "<BlogData: #{title}>"
         end
     end
 
