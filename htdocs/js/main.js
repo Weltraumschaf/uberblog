3c0d1783c593f852f339d91f3dd9007265241d78
Weltraumschaf <ich@weltraumschaf.de>
2012-03-25 18:07:34 +0200

    Add jquery and lab js.

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,27 +1,44 @@
 (function(){
-    var _gaq = _gaq || [], ga, s;
 
-    try {
-        _gaq.push(['_setAccount', 'UA-9617079-2']);
-        _gaq.push(['_gat._anonymizeIp']);
-        _gaq.push(['_trackPageview']);
-        ga = document.createElement('script');
-        ga.type  = 'text/javascript';
-        ga.async = true;
+    function initGoogleAnalytics() {
+        var _gaq = _gaq || [], ga, s;
 
-        if ('https:' == document.location.protocol) {
-            ga.src = 'https://ssl.google-analytics.com/ga.js';
-        }
-        else {
-            ga.src = 'http://www.google-analytics.com/ga.js';
-        }
+        try {
+            _gaq.push(['_setAccount', 'UA-9617079-2']);
+            _gaq.push(['_gat._anonymizeIp']);
+            _gaq.push(['_trackPageview']);
+            ga = document.createElement('script');
+            ga.type  = 'text/javascript';
+            ga.async = true;
 
-        s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
-    } catch(err) {
-        if (window.console && window.console.log) {
-            console.log('exception throwed while GA-Tracking of type: ' +
-                        err.type + ' and message: ' + err.message);
+            if ('https:' == document.location.protocol) {
+                ga.src = 'https://ssl.google-analytics.com/ga.js';
+            }
+            else {
+                ga.src = 'http://www.google-analytics.com/ga.js';
+            }
+
+            s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(ga, s);
+        } catch(err) {
+            if (window.console && window.console.log) {
+                console.log('exception throwed while GA-Tracking of type: ' +
+                            err.type + ' and message: ' + err.message);
+            }
         }
     }
+
+    function loadDependencies(onReadyFn) {
+        $LAB.script('js/jquery-1.7.2.js')
+            .wait(onReadyFn)
+    }
+
+    function main() {
+        loadDependencies(function() {
+
+        });
+        initGoogleAnalytics();
+    }
+
+    main();
 })()
